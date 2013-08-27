6dbdb71431739b4c4c09dc6132c087ef49dede3b
inframatic <isteffy@hotmail.com>
2013-08-27 14:08:07 -0400

    all

---
+++ 04_pig_latin/pig_latin.rb
@@ -0,0 +1,15 @@
+VOWELS = %w{ a e i o u }
+
+
+def translate(word)
+	if VOWELS.include?(word[0])
+		vowel = word + word[0] + "y"
+		return vowel
+	elsif !VOWELS.include?(word[0]) && !VOWELS.include?(word[1])
+		two_consonants = word + word[0] + word[1] + "ay"
+		return two_consonants[2..-1]
+	else 	
+		consonant = word + word[0] + "ay"
+		return consonant[1..-1]
+	end
+end
