7d480ab48fae825260d3d2a4ed887540f94eda1f
inframatic <isteffy@hotmail.com>
2013-08-27 15:46:42 -0400

    all

---
+++ 07_book_titles/untitled.rb
@@ -0,0 +1,18 @@
+class Book
+
+	UNWANTED_WORDS = %w{ the a an and in of }
+
+	attr_accessor :title
+
+	
+	def title
+		words = @title.split(" ")
+		words.map do |word| 
+			word.capitalize! unless UNWANTED_WORDS.include?(word) 
+		end
+		words.first.capitalize! 
+		words.join(" ")
+		end	
+	end	
+end
+
